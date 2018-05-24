70c06ef92a110e60070020b57bc347365eb47e57
Ronnie Miksch <fakepreferences@gmail.com>
2018-05-24 16:08:00 -0700

    Personal bookmarks w logins

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -10,3 +11,6 @@ class Bookmark(models.Model):
   notes = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
